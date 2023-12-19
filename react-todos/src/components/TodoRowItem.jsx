2879c5fa5f897dd16785f8895ea2abc88bbeaa3a
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-19 14:17:03 +0530

    updated files: todo app item list

---
+++ react-todos/src/components/TodoRowItem.jsx
@@ -0,0 +1,13 @@
+function TodoRowItem({ todoNum, todoDescription, todoAssigned }) {
+  return (
+    <>
+      <tr>
+        <th scope="row">{todoNum}</th>
+        <td>{todoDescription}</td>
+        <td>{todoAssigned}</td>
+      </tr>
+    </>
+  );
+}
+
+export default TodoRowItem;
